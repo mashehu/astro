6a5da11a6c6920e00bd0a74eb5a256b6e0b550d2
Nate Moore <nate@astro.build>
2023-11-02 00:12:59 -0500

    feat(assets): move asset propagation logic to internals

--- packages/astro/src/core/build/internal.ts
+++ packages/astro/src/core/build/internal.ts
@@ -81,6 +81,9 @@ export interface BuildInternals {
 	 */
 	discoveredScripts: Set<string>;
 
+	propagatedStylesMap: Map<string, Set<StylesheetAsset>>;
+	propagatedScriptsMap: Map<string, Set<string>>;
+
 	// A list of all static files created during the build. Used for SSR.
 	staticFiles: Set<string>;
 	// The SSR entry chunk. Kept in internals to share between ssr/client build steps
@@ -115,6 +118,9 @@ export function createBuildInternals(): BuildInternals {
 		pageOptionsByPage: new Map(),
 		pagesByViteID: new Map(),
 		pagesByClientOnly: new Map(),
+
+		propagatedStylesMap: new Map(),
+		propagatedScriptsMap: new Map(),
 
 		discoveredHydratedComponents: new Map(),
 		discoveredClientOnlyComponents: new Map(),
